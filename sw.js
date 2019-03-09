871196c1b532c82187c76f8e13624d116c9f95a2
lulzneko <lulzneko@riotz.works>
2019-03-09 14:11:51 +0900

    Update Site [ci skip]

--- sw.js
+++ sw.js
@@ -4,19 +4,19 @@ importScripts('/hairsaronApp/static/workbox.dev.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/hairsaronApp/static/app.e88a0af0005f6ca27d62.js",
-    "revision": "964215054859fd20f5c095bb7dd3480c"
+    "url": "/hairsaronApp/static/app.be74869a460fe16cdc41.js",
+    "revision": "9d5de495df4768fc094d136b1d5e27c1"
   },
   {
-    "url": "/hairsaronApp/static/layouts/default.7f8bbb550eef3e0e86c2.js",
-    "revision": "3458a17008473b6a5f9ff7d5d7779184"
+    "url": "/hairsaronApp/static/layouts/default.36cf0291ad15a138b589.js",
+    "revision": "8d8c9015df8ed2ab95d73e68db91edbd"
   },
   {
-    "url": "/hairsaronApp/static/manifest.d394d8f763ae2dc9442d.js",
-    "revision": "4d485c0ec1225b0125c2a5f9ec7825ae"
+    "url": "/hairsaronApp/static/manifest.86adea0b591807470e86.js",
+    "revision": "d9eb5a9b8ae330e09738747d8ee4cbcf"
   },
   {
-    "url": "/hairsaronApp/static/pages/index.63881d3c885d0822403c.js",
+    "url": "/hairsaronApp/static/pages/index.634f1048d6a9d766de42.js",
     "revision": "0c35efcb00f4ea079b37ff95366494c7"
   },
   {
