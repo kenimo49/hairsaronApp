d4f0d398e3b817106e9c8de1be5e11caca08d045
lulzneko <lulzneko@riotz.works>
2019-03-09 14:41:31 +0900

    ✨ Introduce top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <section>
+  <section class="bottom">
+
+      <!-- ボタン表示 -->
+      <v-layout row>
+        <v-flex xs12>
+          <v-form>
+            <v-text-field label="ユーザー名" v-model="username" required />
+            <v-text-field label="パスワード" v-model="password" :type="show ? 'text' : 'password'" :append-icon="show ? 'visibility' : 'visibility_off'" @click:append="show = !show" />
+            <v-btn flat @click="signin">ログイン</v-btn>
+          </v-form>
+        </v-flex>
+      </v-layout>
+
   </section>
 </template>
 
@@ -8,9 +20,22 @@
 import Vue from 'vue';
 
 export default Vue.extend({
+  data: (): object => ({
+    show: false
+  }),
+  methods: {
+    signin(): void {
+      this.$router.push({ path: '/orders' });
+    }
+  }
 });
 </script>
 
 
 <style scoped>
+.bottom {
+  background-color: rgba(248, 242, 242, .4);
+  padding: 0 20px;
+  color: black;
+}
 </style>
